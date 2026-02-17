afb5d79e63ca130368c6d217c8ddeaf4d65c99d6
Rocketpowered-tech <chaserob12345@gmail.com>
2026-02-16 21:22:18 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -25,9 +25,10 @@ export function score(rank, percent, minPercent) {
 
     and (-24.9975*Math.pow(rank-1, 0.4) + 250)
     -22*Math.pow(rank-1, 0.45) + 250
+    500 - Math.sqrt(rank - 1) * 35
     */
     // New formula
-    let score = (500 - Math.sqrt(rank - 1) * 35) *
+    let score = ((500 - Math.sqrt(rank - 1) * 35) / 2) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
