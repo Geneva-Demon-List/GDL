03d3570e05be8d7bfd79e6338f4c54ef2ec161e9
Rocketpowered-tech <chaserob12345@gmail.com>
2026-04-10 19:32:11 -0400

    Repositioned the description

--- js/pages/List.js
+++ js/pages/List.js
@@ -39,9 +39,9 @@ export default {
             <div class="level-container">
                 <div class="level" v-if="level">
                     <h1>{{ level.name }}</h1>
+                    <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <p v-if="level.description">{{level.description}}</p>
                     <p v-else>Description not found.</p>
-                    <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
                     <ul class="stats">
                         <li>
