e565a465347718727e123787557e30b4c605c591
Rocketpowered-tech <chaserob12345@gmail.com>
2026-04-15 16:42:30 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 1;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 200) {
+    if (rank > 150) {
         return 0;
     }
-    if (rank > 100 && percent < 100) {
+    if (rank > 75 && percent < 100) {
         return 0;
     }
 
